568bcfe7837f5f72f23b3e85fce3e8e08d190e46
maxyee <eyaminkhan00@gmail.com>
2021-07-19 23:29:00 +0600

    data added to redis and related code

--- redis-node-cache/README.md
+++ redis-node-cache/README.md
@@ -92,3 +92,72 @@ app.listen(5000, () => {
 - finised the primary stage.
 
 ## Add Redis Code
+
+```js
+// index.js
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+// Set response
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} Github repos</h2>`;
+}
+
+// Make request to Github for data
+async function getRepos(req, res, next) {
+  try {
+    console.log("Fetching Data...");
+
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    // Set data to Redis
+    client.setex(username, 3600, repos);
+
+    res.send(setResponse(username, repos));
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
+}
+
+// Cache middleware
+function cache(req, res, next) {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  });
+}
+
+app.get("/repos/:username", cache, getRepos);
+
+app.listen(5000, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+```
+
+- Finally check the response time from google chrome network section
+
+- When we put middleware `cache` in request api. we see that it took less time.
+
+- on the other hand, if we don't use the `cache` middleware it took a lot's of time.
--- redis-node-cache/index.js
+++ redis-node-cache/index.js
@@ -6,27 +6,54 @@ const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
+
 const app = express();
 
-// Make request to github for request data.
-async function getRepos (req,res, next){
-    try{
-        console.log('Fetching repositories');
-
-        const { username } = req.params;
-
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const data = await response.json();
-
-        res.send(data);
-    }catch(err){
-        console.error(err);
-        res.status(500);
-    }
+// Set response
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} Github repos</h2>`;
 }
 
-app.get('/repos/:username', getRepos);
+// Make request to Github for data
+async function getRepos(req, res, next) {
+  try {
+    console.log('Fetching Data...');
+
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    // Set data to Redis
+    client.setex(username, 3600, repos);
+
+    res.send(setResponse(username, repos));
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
+}
+
+// Cache middleware
+function cache(req, res, next) {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  });
+}
+
+app.get('/repos/:username', cache, getRepos);
 
 app.listen(5000, () => {
-    console.log(`App listening of port ${PORT}`)
+  console.log(`App listening on port ${PORT}`);
 });
